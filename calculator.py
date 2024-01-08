3c8c226b300615ef78bcaca1fecb700ecaba3cb5
Nishan Bastola <nishanbastola9095@gmail.com>
2024-01-08 20:11:43 +0545

    calculator

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#calculator using python.
+print(''''
++ADD
+-SUBTRACT
+*MULTIPLY
+/DIVIDE
+''') #triple maa multiple line print garna milcha.
+num1=int(input("enter the num1"))
+num2=int(input("enter the num2"))
+opr=5(input("enter the operator"))
+if opr=="+":
+    print(num1+num2)
+elif opr=="-":
+    print(num1-num2)
+elif opr=="*":
+    print(num1*num2)
+elif opr=="/":
+    print(num1/num2)
+else:
+    print("invalid operator.")
+
+
