ce4ad08c42bab060995ba991bae5711ba39d5e1b
Nishan Bastola <83748435+Nishan-coder@users.noreply.github.com>
2024-09-14 12:29:32 +0545

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,19 +1,25 @@
-l1=[x for x in range(1,101)]
+l1 = [x for x in range(1, 101)]
 print(l1)
-item_to_be_searched=int(input('enter the number to be searched'))
-l=0
-r=len(l1)-1
-count =0
-while l<r:
-    print('middle term:',l1[(l+r)//2])
-    if item_to_be_searched==l1[(l+r)//2]:
-        print('Item found')
-        print('Count:',count)
+item_to_be_searched = int(input('Enter the number to be searched: '))
+l = 0
+r = len(l1) - 1
+count = 0
+
+while l <= r:
+    mid = (l + r) // 2
+    print('Middle term:', l1[mid])
+    
+    if item_to_be_searched == l1[mid]:
+        print('Item found at index:', mid)
+        print('Count:', count)
         break
-    elif item_to_be_searched<(l+r)//2:
-            r=(l+r)//2-1
+    elif item_to_be_searched < l1[mid]:
+        r = mid - 1
     else:
-        l=(l+r)//2+1
-    count+=1
-if(l>=r):
-    print('item not found')
+        l = mid + 1
+    
+    count += 1
+
+if l > r:
+    print('Item not found')
+    print('Count:', count)
